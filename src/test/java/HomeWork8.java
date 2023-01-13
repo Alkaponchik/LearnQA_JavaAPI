e9698a847617c3b8ae8b1676e7a20b287355ae4a
Alkapochick <609716@mail.ru>
2023-01-13 19:45:24 +1000

    HomeWork8

---
+++ src/test/java/HomeWork8.java
@@ -0,0 +1,52 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.Test;
+
+import java.util.Objects;
+
+public class HomeWork8 {
+    @Test
+    public void HomeWork8 () throws InterruptedException {
+        JsonPath responseTokenAndCreateTusk = RestAssured  //создаем задачу и получаем токен
+                .given()
+                .when()
+                .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                .jsonPath();
+
+        String responsesToken = responseTokenAndCreateTusk.get("token"); //кладем токен в переменную
+        int responseTime = responseTokenAndCreateTusk.get("seconds"); //кладем время в переменную
+        System.out.println(responseTime);
+
+        JsonPath getTuskStatus = RestAssured  //проверяем статус по задаче согласно токену
+                .given()
+                .queryParams("token", responsesToken)
+                .when()
+                .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                .jsonPath();
+        System.out.println("Задача создана");
+        String responsesStatus = getTuskStatus.get("status");
+
+        String shittyString = "Job is NOT ready";
+        System.out.println("Статус выполнения задачи: "+ responsesStatus);
+
+        if (Objects.equals(responsesStatus, shittyString)) {
+            Thread.sleep(responseTime * 1000L);
+
+            JsonPath putTuskTime = RestAssured //Повторный вызов спустя время
+                    .given()
+                    .queryParams("token", responsesToken)
+                    .when()
+                    .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                    .jsonPath();
+            String status3 = putTuskTime.get("status");
+            System.out.println("Статус выполнения задачи: "+ status3);
+
+            String resultPutTuskTime = putTuskTime.get("result");
+            System.out.println("Результат выполненной задачи: "+ resultPutTuskTime);
+
+        }  else {
+            String result = getTuskStatus.get("status");
+            System.out.println(result);
+        }
+    }
+}
